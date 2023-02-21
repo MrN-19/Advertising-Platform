26ec1e612d10afa2ce5f14082fc707fe18ab2698
Mohammadreza <mohammadreza.mrn.29@gmail.com>
2023-02-22 00:41:54 +0330

    add login url route

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import UserRegister
+from .views import UserRegister,UserLogin
 
 app_name:str = "account"
 
 urlpatterns = [
     path("register",UserRegister.as_view(),name="register"),
+    path("login",UserLogin.as_view(),name="login"),
 ]
