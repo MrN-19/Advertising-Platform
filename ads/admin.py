011bc497eabfe7115e3442749fc998dffcd35a7f
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-03-08 18:14:47 +0330

    add AdsCategoryAdmin and AdsAdmin class into admin.py file for showing in admin panel

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class AdsCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug" : ("title",)}
+
+admin.site.register(models.AdsCategory,AdsCategoryAdmin)
+
+class AdsAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(models.Ads,AdsAdmin)
+
+
