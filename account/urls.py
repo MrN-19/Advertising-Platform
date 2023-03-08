50424134cf6d432526b262867e72823d87455279
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-03-08 18:11:57 +0330

    add marked-ads url path into urls.py file at account app

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import UserRegister,UserLogin
+from .views import UserRegister,UserLogin,UserMarkedAds
 
 app_name:str = "account"
 
 urlpatterns = [
     path("register",UserRegister.as_view(),name="register"),
     path("login",UserLogin.as_view(),name="login"),
+    path("marked-ads",UserMarkedAds.as_view(),name="markedads"),
 ]
