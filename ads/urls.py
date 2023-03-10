f506b42a4091cb29cdc20c3f6417108c480cf65a
Mohammadreza Nazif <mohammadreza.mrn.29@gmail.com>
2023-03-10 18:42:41 +0330

    add some url paths

--- ads/urls.py
+++ ads/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import AdsByCategory
+from .views import AdsByCategory,AllAds,AdsByCityProvince,AllCategories,NewAds
 
 app_name:str = "ads"
 
 urlpatterns = [
+    path("",AllAds.as_view(),name="allads"),
+    path("categories",AllCategories.as_view(),name="allcategories"),
     path("category/<str:slug>",AdsByCategory.as_view(),name="adsbycategory"),
+    path("<str:city_province>",AdsByCityProvince.as_view(),name="adsbycity"),
+    path("new/",NewAds.as_view(),name="newads"),
 ]
